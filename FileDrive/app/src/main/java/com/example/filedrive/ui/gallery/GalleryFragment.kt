bf1a07e3b0f40fc292eeda59bac9f854d1878b2a
faheem4531 <faheem4531@gmail.com>
2023-12-28 18:57:52 +0500

    side cases

--- FileDrive/app/src/main/java/com/example/filedrive/ui/gallery/GalleryFragment.kt
+++ FileDrive/app/src/main/java/com/example/filedrive/ui/gallery/GalleryFragment.kt
@@ -1,59 +1,35 @@
 package com.example.filedrive.ui.gallery
 
-import android.net.Uri
 import android.os.Bundle
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.ArrayAdapter
-import android.widget.Button
 import android.widget.ImageView
-import android.widget.LinearLayout
-import android.widget.ListView
 import android.widget.ProgressBar
-import android.widget.TextView
 import android.widget.Toast
-import androidx.activity.result.ActivityResultCallback
-import androidx.activity.result.contract.ActivityResultContracts
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.ViewModelProvider
-import androidx.lifecycle.findViewTreeViewModelStoreOwner
-import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.filedrive.R
 import com.example.filedrive.databinding.FragmentGalleryBinding
 import com.example.filedrive.ui.ImageAdapter
-import com.google.firebase.Firebase
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.database.DataSnapshot
 import com.google.firebase.database.DatabaseError
 import com.google.firebase.database.DatabaseReference
 import com.google.firebase.database.FirebaseDatabase
 import com.google.firebase.database.ValueEventListener
-import com.google.firebase.storage.FirebaseStorage
-import com.google.firebase.storage.storage
 
 class GalleryFragment : Fragment() {
 
-    //storing variables
+    //Declarations
     private lateinit var dbRef: DatabaseReference
-    private  var dbStorage= Firebase.storage
-    private lateinit var uploadImage : ImageView
-    private lateinit var progresGallery : ProgressBar
-
-    private var uri : Uri ?= null
-
-    //fetching declaration
     private lateinit var recyclerView: RecyclerView
     private lateinit var listImages: ArrayList<String>
 
 
 
     private var _binding: FragmentGalleryBinding? = null
-    // This property is only valid between onCreateView and
-    // onDestroyView.
     private val binding get() = _binding!!
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -64,14 +40,14 @@ class GalleryFragment : Fragment() {
         val root: View = binding.root
 
 
-        // Code Starting
-        dbStorage = FirebaseStorage.getInstance()
-        uploadImage  = root.findViewById (R.id.uploadImage)
-        progresGallery = root.findViewById (R.id.uploadImageProgress)
 
+
+        //=>  Code Starting
         recyclerView= root.findViewById (R.id.recycler)
         recyclerView.layoutManager = LinearLayoutManager(requireContext())
         listImages = arrayListOf()
+        var imageLoader = root.findViewById<ProgressBar>(R.id.imageLoader)
+        var noImage = root.findViewById<ImageView> (R.id.noImageFound)
 
 
         // Initialize dbRef after Firebase initialization
@@ -81,73 +57,35 @@ class GalleryFragment : Fragment() {
                 .child("galleryImagesUrl")
         }
 
-
-        var galleryImage = registerForActivityResult(
-            ActivityResultContracts.GetContent(),
-          ActivityResultCallback  {url->
-                url?.let{
-                    uri = url
-                    uploadImage(url)
-                }
-            }
-        )
-
-        uploadImage.setOnClickListener{
-            galleryImage.launch("image/*")
-        }
-
         dbRef.addValueEventListener(object :ValueEventListener{
             override fun onDataChange(snapshot: DataSnapshot) {
                 if (snapshot.exists()){
+                    noImage.visibility = View.GONE
                     for (image in snapshot.children){
                         val currentImage = image.getValue(String::class.java)
                         listImages.add(currentImage!!)  // !! means shouldn't be null
                     }
+                    imageLoader.visibility= View.GONE
                     recyclerView.adapter = ImageAdapter (requireContext(), listImages)
+                }
+                else{
+                    imageLoader.visibility= View.GONE
+                    noImage.visibility = View.VISIBLE
                 }
             }
 
             override fun onCancelled(error: DatabaseError) {
                 Toast.makeText(requireContext(), error.toString(), Toast.LENGTH_SHORT).show()
             }
-
         })
+
+
 
         return root
     }
 
-    private fun uploadImage(imageUri: Uri) {
-
-    imageUri?.let {
-            uploadImage.visibility = View.GONE
-            progresGallery.visibility = View.VISIBLE
-
-            var userId = FirebaseAuth.getInstance().currentUser?.uid
-
-            dbStorage.getReference("Gallery Images").child(userId.toString())
-                .child(System.currentTimeMillis().toString())
-                .putFile(imageUri)
-                .addOnSuccessListener { task ->
-                    task.metadata?.reference?.downloadUrl?.addOnSuccessListener { url ->
-                        uri = url
-                        Toast.makeText(requireContext(), "upload sucess", Toast.LENGTH_SHORT).show()
 
 
-                        //store image url in realTime database
-                        dbRef.push().setValue(uri.toString()).addOnFailureListener {
-                            Toast.makeText(requireContext(), it.message.toString(), Toast.LENGTH_SHORT).show()
-                        }
-
-                        uploadImage.visibility = View.VISIBLE
-                        progresGallery.visibility = View.GONE
-                    }
-                        ?.addOnFailureListener {
-                            Toast.makeText(requireContext(), it.message.toString(), Toast.LENGTH_SHORT)
-                                .show()
-                        }
-                }
-        }
-    }
 
     override fun onDestroyView() {
         super.onDestroyView()
